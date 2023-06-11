a535d0545c6bd0694abaef33fef1fa53cc18b0d6
khuangaf <huangkh19951228@gmail.com>
2023-06-11 15:24:22 -0500

    fixed bug in qa

--- zerofec/qa/run_qa_unified.py
+++ zerofec/qa/run_qa_unified.py
@@ -22,20 +22,23 @@ samples = [json.loads(l) for l in open(args.generated_question_path,'r').readlin
 
 
 for sample in tqdm(samples, desc="Generating Answers"):
-
+    
     generated_questions = sample['generated_question']
     sample['answer'] = []
     for question in generated_questions:
         
         
         answer_predicted = 0
+        question_answers = []
         for ctx_idx, ctx in enumerate(sample['evidence_text']):
+            
             current_answers = []
 
             words = word_tokenize(ctx)
             passage_size = 400
             for i in range(0, len(words), passage_size):
                 context = ' '.join(words[i:i+passage_size])
+                
                 input_ids = tokenizer.encode(f"{question} \n {context}", return_tensors='pt').cuda()
                 
                 with torch.no_grad():
@@ -44,8 +47,8 @@ for sample in tqdm(samples, desc="Generating Answers"):
                     predict_answer_tokens_string = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
                     
                     current_answers.append(predict_answer_tokens_string.strip())
-                    
-        sample['answer'].append(current_answers)
+            question_answers.append(current_answers)  
+        sample['answer'].append(question_answers)
     
 
 with open(args.output_path ,'w') as f:
